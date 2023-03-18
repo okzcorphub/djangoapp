51aa7f8eec0702e8acfbae13730253376afde03d
Onyeocha Favour <97852822+FavourEva@users.noreply.github.com>
2023-03-18 21:52:41 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "circleci-eks-${random_string.suffix.result}"
+  cluster_name = "exam-cluster"
 }
 
 resource "random_string" "suffix" {
